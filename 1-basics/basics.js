768287e103a41731a1741ffc2679ed3abf63d045
Paul Ngugi <paulngugz@gmail.com>
2019-10-31 13:49:12 +0300

    added falsey number

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -280,7 +280,8 @@ delete object.someProperty;
             ////////////////////////////////////////////////////////////////////
             // Can you find two different numbers that each cast to `false` in an if() statement?
             // ✏️ ADD CODE HERE ✏️
-
+            const falsyNumber1 = 0;
+            const falsyNumber2 = NaN
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof falsyNumber1).to.equal("number");
